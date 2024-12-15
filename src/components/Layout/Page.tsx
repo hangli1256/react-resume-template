e8e1d912e3a71e4f1ae78f18e1c57b0be597123b
hangli1256 <50407352+hangli1256@users.noreply.github.com>
2024-12-15 17:16:19 +0100

    add speed insight

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,5 +1,5 @@
 import {Analytics} from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import {SpeedInsights} from "@vercel/speed-insights/next"
 import {NextPage} from 'next';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
