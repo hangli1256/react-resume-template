e1e98d3d5a0af6c53e18f14eb2c7aa9a15076fe2
hangli1256 <50407352+hangli1256@users.noreply.github.com>
2024-12-15 16:57:39 +0100

    Update Page.tsx

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,8 +1,9 @@
+import {Analytics} from '@vercel/analytics/next';
 import {NextPage} from 'next';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
 import {memo, PropsWithChildren} from 'react';
-import { Analytics } from '@vercel/analytics/next';
+
 import {HomepageMeta} from '../../data/dataDef';
 
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
